bfbb99294a2aa1986442f7ee3a16bb7f8bd5baa0
Victor Gonçalves de Andrade <0vetor0@gmail.com>
2021-04-19 19:23:10 -0300

    SellerDao and DaoFactory
    Both classes are deployed.

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class Program {
 		Department dep = new Department("Books",1); 
 		
 		Seller sel = new Seller("Ruan", 1, "ruan@gmail.com", new Date(), 3000.0, dep);
+		
+		// Injeção de dependência sem explicitar a implementação
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(sel);
 
