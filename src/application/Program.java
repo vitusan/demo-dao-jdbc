76c19fcf50472566fd3c7a649612092c06c1dbbe
Victor Gonçalves de Andrade <0vetor0@gmail.com>
2021-04-19 15:52:29 -0300

    Department Entity Class
    Department Class deployed.

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department dep = new Department("Books",1); 
+		
+		System.out.println(dep);
+
+	}
+
+}
---
+++ src/entities/Department.java
@@ -0,0 +1,67 @@
+package entities;
+
+import java.io.Serializable;
+
+public class Department implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private Integer id;
+	
+	public Department() {
+		
+	}
+	
+	public Department(String name, Integer id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Department - " + name + ", id =" + id + ".";
+	}
+	
+}
