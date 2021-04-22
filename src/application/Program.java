8e4cda3159c08d154b7912e5f6be8b873f4dda9f
Victor Gonçalves de Andrade <0vetor0@gmail.com>
2021-04-22 17:09:30 -0300

    Implemetation of findById() method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,19 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Department dep = new Department("Books",1); 
-		
-		Seller sel = new Seller("Ruan", 1, "ruan@gmail.com", new Date(), 3000.0, dep);
 		
 		// Injeção de dependência sem explicitar a implementação
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(sel);
+		Seller seller = sellerDao.findById(2);
+		
+		System.out.println(seller);
 
 	}
 
